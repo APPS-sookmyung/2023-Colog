af0100028b8f3afafdbcdbad75467df83fb532d3
ajeong <ajung7038@naver.com>
2023-11-25 05:41:57 +0900

    Feat: Sidebar Timer 구현

--- src/pages/sidebar/SidebarLog.js
+++ src/pages/sidebar/SidebarLog.js
@@ -6,12 +6,16 @@ import profileImg from "../../assets/img/profile_photo.png";
 import topBackground from "../../assets/background/top_background.png";
 import line from "../../assets/background/line_group.png";
 import topEclipse from "../../assets/background/top_eclipse.png";
+import start_button from "../../assets/icon/start_button.png";
+import pause_button from "../../assets/icon/pause_button.png";
+import stop_button from "../../assets/icon/stop_button.png";
 
 const SidebarLog = () => {
   return (
-    <div className="SidebarLog">
+    <div className="sidebarLog">
       <Study />
       <ToDo />
+      <Timer />
     </div>
   );
 };
@@ -24,8 +28,8 @@ const Study = () => {
     <div className="sidebarLogStudy">
       <img src={topEclipse} className="topEclipse" />
       <a href="/mypage" style={{ color: "black", textDecoration: "none" }}>
-        <div className="SidebarLogStudyTitle">STUDY</div>
-        <div className="SliderLogLine">
+        <div className="sidebarLogStudyTitle">STUDY</div>
+        <div>
           <img src={line} />
         </div>
         <br />
@@ -46,8 +50,8 @@ const ToDo = () => {
     <div className="sidebarLogTodo">
       <img src={topEclipse} className="topEclipse" />
       <a href="/mypage" style={{ color: "black", textDecoration: "none" }}>
-        <div className="SidebarLogStudyTitle">TODO</div>
-        <div className="SliderLogLine">
+        <div className="sidebarLogStudyTitle">TODO</div>
+        <div>
           <img src={line} />
         </div>
         <br />
@@ -58,6 +62,32 @@ const ToDo = () => {
           </div>
         </div>
       </a>
+    </div>
+  );
+};
+
+// Timer
+const Timer = () => {
+  return (
+    <div className="sidebarLogTimer">
+      <img src={topEclipse} className="topEclipse" />
+      <a href="/mypage" style={{ color: "black", textDecoration: "none" }}>
+        <div className="sidebarLogTimerTitle">TIMER</div>
+        <div className="sliderLogLineTimer">
+          <img src={line} alt="Line" />
+        </div>
+      </a>
+      <div className="sidebarLogButtons">
+        <button>
+          <img src={start_button} alt="Start" className="sidebarLogButton" />
+        </button>
+        <button>
+          <img src={pause_button} alt="Pause" className="sidebarLogButton" />
+        </button>
+        <button>
+          <img src={stop_button} alt="Stop" className="sidebarLogButton" />
+        </button>
+      </div>
     </div>
   );
 };
