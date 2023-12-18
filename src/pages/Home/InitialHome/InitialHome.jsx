a082bda483493ca8635c220bb7da98235aaf63c7
ajeong <ajung7038@naver.com>
2023-12-18 21:36:46 +0900

    Feat: Home 컴포넌트 분리

--- src/pages/Home/InitialHome/InitialHome.jsx
+++ src/pages/Home/InitialHome/InitialHome.jsx
@@ -15,27 +15,28 @@ const InitialHome = () => {
           <S.firstDescriptionText>코딩을 기록하다</S.firstDescriptionText>
         </S.Logo>
       </S.firstContainer>
-      <S.secondContainer>
-        {/* <S.BackgroundImage src={BackgroundImage} alt="BackgroundImage" /> */}
-        <S.secondDescription1>
-          뿌듯함을 채워주고 성취감을 극대화하는
-        </S.secondDescription1>
-        <S.secondDescription2>프로그래머 전용 타이머 앱</S.secondDescription2>
-      </S.secondContainer>
-      <S.thridContainer>
-        <S.thirdDescription1>
-          프로그래머만의 뿌듯함을 느끼고 싶으신가요?
-        </S.thirdDescription1>
-        <S.thirdDescription2>
-          나만의 코딩 기록 서비스, 코록은 타이머, 뽀모도로 등
-          <br />
-          집중을 위한 최적의 환경을 제공하고 월 별 통계를 통해
-          <br />
-          성취감을 극대화합니다. 코딩 시간, 기술 블로그 업로드 등
-          <br />
-          코딩 기록을 한 곳에 모아 보세요!
-        </S.thirdDescription2>
-      </S.thridContainer>
+      <S.secondThridContainer>
+        <S.secondContainer>
+          <S.secondDescription1>
+            뿌듯함을 채워주고 성취감을 극대화하는
+          </S.secondDescription1>
+          <S.secondDescription2>프로그래머 전용 타이머 앱</S.secondDescription2>
+        </S.secondContainer>
+        <S.thridContainer>
+          <S.thirdDescription1>
+            프로그래머만의 뿌듯함을 느끼고 싶으신가요?
+          </S.thirdDescription1>
+          <S.thirdDescription2>
+            나만의 코딩 기록 서비스, 코록은 타이머, 뽀모도로 등
+            <br />
+            집중을 위한 최적의 환경을 제공하고 월 별 통계를 통해
+            <br />
+            성취감을 극대화합니다. 코딩 시간, 기술 블로그 업로드 등
+            <br />
+            코딩 기록을 한 곳에 모아 보세요!
+          </S.thirdDescription2>
+        </S.thridContainer>
+      </S.secondThridContainer>
       <S.fourthContainer>
         <S.startButton>START !</S.startButton>
         <S.copyRightText>Copyrightⓒ2023 Ajeong</S.copyRightText>
--- src/pages/Home/InitialHome/InitialHome.style.jsx
+++ src/pages/Home/InitialHome/InitialHome.style.jsx
@@ -1,6 +1,8 @@
 // InitialHome.style.jsx
 import styled from "@emotion/styled";
 
+import BackgroundImage from "../../../assets/img/Colog.background.png";
+
 export const InitialHome = styled.div``;
 
 // firstContainer
@@ -28,13 +30,37 @@ export const LogoText = styled.div`
 export const firstDescriptionText = styled.div``;
 
 // secondContainer
-export const secondContainer = styled.div``;
-export const BackgroundImage = styled.img``;
-export const secondDescription1 = styled.div``;
-export const secondDescription2 = styled.div``;
+export const secondThridContainer = styled.div`
+  background-image: url("${BackgroundImage}");
+  background-size: cover;
+  width: 100%;
+  height: 2160px;
+  align-items: center;
+  box-shadow: inset -200px 0 0 0 #6e6f70, inset 200px 0 0 0 #6e6f70;
+
+  // display: flex;
+  margin: 0 auto;
+`;
+// secondContainer
+export const secondContainer = styled.div`
+  height: 50%;
+`;
+export const secondDescription1 = styled.div`
+  color: white;
+  font-size: 20px;
+`;
+export const secondDescription2 = styled.div`
+  color: white;
+  font-size: 50px;
+`;
 
 // thridContainer
-export const thridContainer = styled.div``;
+export const thridContainer = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 100%;
+  height: 50%;
+  flex-shrink: 0;
+`;
 export const thirdDescription1 = styled.div``;
 export const thirdDescription2 = styled.div``;
 
