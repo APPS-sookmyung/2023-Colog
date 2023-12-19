73a39cf09ee3d8ddcbc70a7b8c27e15d5a4dc471
ajeong <ajung7038@naver.com>
2023-12-19 11:03:47 +0900

    Feat: Sidebar 구현

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import InitialHome from "./pages/Home/InitialHome/InitialHome";
 import TimerPage from "./pages/Home/TimerPage/TimerPage";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
+      {/* <Header />
+      <InitialHome /> */}
       {/* <TimerPage /> */}
-      {/* <InitialHome /> */}
-      <Header />
+      <Sidebar />
     </div>
   );
 };
---
+++ src/components/Sidebar/Sidebar.style.jsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const Sidebar = styled.div`
+  width: 297px;
+  height: 429px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const SidebarProfileImage = styled.img``;
+export const SidebarName = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+`;
+export const SidebarLine = styled.div`
+  width: 80%;
+  height: 5px;
+  border-radius: 5px;
+  background-color: black;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+export const SidebarIntroduce = styled.div`
+  font-size: 15px;
+  margin-bottom: 10px;
+`;
+export const SidebarLink = styled.div``;
