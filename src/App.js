57f977a676af8b91aa5319953b60d6a0c0e3518d
ajeong <ajung7038@naver.com>
2023-12-19 11:10:48 +0900

    Del: delete TimerPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import InitialHome from "./pages/Home/InitialHome/InitialHome";
-import TimerPage from "./pages/Home/TimerPage/TimerPage";
+import Timer from "./components/Timer/Timer";
+import CountDown from "./components/CountDown/CountDown";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
-      {/* <Header />
-      <InitialHome /> */}
-      {/* <TimerPage /> */}
-      <Sidebar />
+      {/* <Header /> */}
+      {/* <InitialHome /> */}
+      <Timer />
+      <CountDown initialHours={0} initialMinutes={0} initialSeconds={3} />
+      {/* <Sidebar /> */}
     </div>
   );
 };
--- src/pages/Home/TimerPage/TimerPage.style.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const TimerPage = styled.div`
-  // styles for the header
-`;
-
-export const Timer = styled.div``;
-
-export const CountDown = styled.div``;
