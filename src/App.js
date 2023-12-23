8d7f2ef2b9e40f8a25655323761cbd762e838d1f
ajeong <ajung7038@naver.com>
2023-12-23 14:38:03 +0900

    Feat: 월 별 공부량 통계 차트 기본 틀 구현

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import InitialHome from "./pages/Home/InitialHome/InitialHome";
 import TimerPage from "./pages/TimerPage/TimerPage";
 import PomodoroPage from "./pages/PomodoroPage/PomodoroPage";
 import ToDoListPage from "./pages/ToDoListPage";
-import TotalStudyTime from "./components/TotalStudy/TotalStudy";
 import CalendarPage from "./pages/CalendarPage/CalendarPage";
-import Calendar from "./components/Calendar/Calendar";
+import MonthlyStats from "./components/MonthlyStats/MonthlyStats";
 
 const App = () => {
   return (
     <Router>
-      {/* <Calendar /> */}
-      <Routes>
+      <MonthlyStats />
+      {/* <Routes>
         <Route path="/" element={<InitialHome />} />
         <Route path="/timer" element={<TimerPage />} />
         <Route path="/pomodoro" element={<PomodoroPage />} />
         <Route path="/todolist" element={<ToDoListPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
-      </Routes>
+      </Routes> */}
     </Router>
   );
 };
---
+++ src/components/MonthlyStats/MonthlyStats.style.jsx
@@ -0,0 +1,5 @@
+// MonthlyStats.style.jsx
+
+import styled from "@emotion/styled";
+
+export const MonthlyStats = styled.div``;
