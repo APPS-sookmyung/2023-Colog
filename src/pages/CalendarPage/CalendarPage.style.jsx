8a8c1a655334e44958c317647afb8fef558806c4
ajeong <ajung7038@naver.com>
2023-12-24 19:51:03 +0900

    Feat: CalendarPage CSS 디테일 추가

--- src/pages/CalendarPage/CalendarPage.style.jsx
+++ src/pages/CalendarPage/CalendarPage.style.jsx
@@ -1,12 +1,12 @@
 import styled from "@emotion/styled";
 
-export const CalendarPage = styled.div``;
+export const CalendarPage = styled.div`
+  margin-top: 10%;
+`;
+
 export const Content = styled.div`
   display: flex;
-  justify-content: space-between;
-  //   justify-content: center;
-  margin: 5%;
-  margin-left: 15%;
-  margin-right: 15%;
   align-items: center;
+  justify-content: center;
+  gap: 100px;
 `;
