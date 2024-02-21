2ff53bda904ba3d64e7c561be66f879c63bef37a
ajeong <ajung7038@naver.com>
2024-02-21 11:12:47 +0900

    Fix: ToDoList 컴포넌트 크기 조정 #20

--- src/components/ToDoList/ToDoList.style.jsx
+++ src/components/ToDoList/ToDoList.style.jsx
@@ -16,11 +16,21 @@ export const ToDoListTitle = styled.div`
   padding: 20px;
   letter-spacing: 5px;
   color: #61a5ff;
+
+  @media all and (max-width: 500px) {
+    font-size: 60px;
+  }
 `;
 export const ToDoListContent = styled.div`
   font-family: SeoulHangang;
   font-size: 20px;
   color: #585858;
+
+  @media all and (max-width: 500px) {
+    font-size: 16px;
+    gap: 5px;
+    line-height: 25px;
+  }
 `;
 export const ToDoListItem = styled.div`
   &:hover {
