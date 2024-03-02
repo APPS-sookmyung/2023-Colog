378a60a7a0d30981a3b515ff827d3adaeb63c2e4
ajeong <ajung7038@naver.com>
2024-03-02 11:31:28 +0900

    Feat: 일일 통계 공부 시간 불러오기 기능 구현 #32

--- src/components/CustomDate/CustomDate.jsx
+++ src/components/CustomDate/CustomDate.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const CustomDate = () => {
   const today = new Date(); // 현재 날짜 불러오기
 
-  const formattedDate = `${today.getFullYear()}.${
-    today.getMonth() + 1 < 10 ? "0" : ""
-  }${today.getMonth() + 1}.${today.getDate() < 0 ? "0" : ""}${today.getDate()}`;
-  return <>{formattedDate}</>;
+  const formattedDate = `${today.getFullYear()}${
+    today.getMonth() + 1 < 10 ? 0 : ""
+  }${today.getMonth() + 1}${today.getDate() < 0 ? 0 : ""}${today.getDate()}`;
+  return formattedDate;
 };
 
 export default CustomDate;
